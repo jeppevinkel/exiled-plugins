2006daf9be790dac4c52bb8e251238fda1d6e172
Jeppevinkel <jeppevinkel@gmail.com>
2020-08-30 23:59:06 +0200

    Fix PluginReleaseSeeder ID mishap.
    Signed-off-by: Jeppevinkel <jeppevinkel@gmail.com>

--- database/seeds/PluginReleaseSeeder.php
+++ database/seeds/PluginReleaseSeeder.php
@@ -17,10 +17,10 @@ class PluginReleaseSeeder extends Seeder
             ['id' => 2, 'exiled_version' => '2.0.1', 'plugin_version' => '1.0.1', 'download_url' => 'www.google.com', 'plugin_id' => 2, 'downloads' => 12, 'created_at' => Carbon::now()->addHours(2)],
             ['id' => 3, 'exiled_version' => '2.1.0', 'plugin_version' => '1.1.0', 'download_url' => 'www.google.com', 'plugin_id' => 2, 'downloads' => 1, 'created_at' => Carbon::now()->addHours(3)],
             ['id' => 4, 'exiled_version' => '2.10.0', 'plugin_version' => '2.0.0', 'download_url' => 'www.google.com', 'plugin_id' => 2, 'downloads' => 37, 'created_at' => Carbon::now()->addHours(4)],
-            ['id' => 1, 'exiled_version' => '2.0.0', 'plugin_version' => '1.0.0', 'download_url' => 'www.google.com', 'plugin_id' => 1, 'downloads' => 13, 'created_at' => Carbon::now()->addHours(1)],
-            ['id' => 2, 'exiled_version' => '2.0.1', 'plugin_version' => '1.0.1', 'download_url' => 'www.google.com', 'plugin_id' => 3, 'downloads' => 12, 'created_at' => Carbon::now()->addHours(2)],
-            ['id' => 3, 'exiled_version' => '2.1.0', 'plugin_version' => '1.1.0', 'download_url' => 'www.google.com', 'plugin_id' => 4, 'downloads' => 1, 'created_at' => Carbon::now()->addHours(3)],
-            ['id' => 4, 'exiled_version' => '2.10.0', 'plugin_version' => '2.0.0', 'download_url' => 'www.google.com', 'plugin_id' => 1, 'downloads' => 37, 'created_at' => Carbon::now()->addHours(4)],
+            ['id' => 5, 'exiled_version' => '2.0.0', 'plugin_version' => '1.0.0', 'download_url' => 'www.google.com', 'plugin_id' => 1, 'downloads' => 13, 'created_at' => Carbon::now()->addHours(1)],
+            ['id' => 6, 'exiled_version' => '2.0.1', 'plugin_version' => '1.0.1', 'download_url' => 'www.google.com', 'plugin_id' => 3, 'downloads' => 12, 'created_at' => Carbon::now()->addHours(2)],
+            ['id' => 7, 'exiled_version' => '2.1.0', 'plugin_version' => '1.1.0', 'download_url' => 'www.google.com', 'plugin_id' => 4, 'downloads' => 1, 'created_at' => Carbon::now()->addHours(3)],
+            ['id' => 8, 'exiled_version' => '2.10.0', 'plugin_version' => '2.0.0', 'download_url' => 'www.google.com', 'plugin_id' => 1, 'downloads' => 37, 'created_at' => Carbon::now()->addHours(4)],
         ];
 
         foreach ($items as $item)
